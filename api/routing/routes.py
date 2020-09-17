b0a7d33914693580bf5d87162aca54d70f755392
mevanmaele <mvanmaele@talusanalytics.com>
2020-09-17 12:26:52 -0400

    Add basic endpoint for getting items with pagination

--- api/routing/routes.py
+++ api/routing/routes.py
@@ -14,7 +14,25 @@ from ..db import api
 from .. import schema
 from ..utils import format_response
 
-# Initialize metric catalog or specifics endpoint
+
+@api.route("/get/items", methods=["GET"])
+class Items(Resource):
+    parser = api.parser()
+    parser.add_argument(
+        'argument_name',
+        type=str,
+        required=False,
+        help="""Description of argument"""
+    )
+
+    @api.doc(parser=parser)
+    @db_session
+    @format_response
+    def get(self):
+        data = schema.get_items()
+        return data
+
+
 @api.route("/test", methods=["GET"])
 class Test(Resource):
     parser = api.parser()
--- api/schema.py
+++ api/schema.py
@@ -22,4 +22,24 @@ from .utils import passes_filters
 # pretty printing: for printing JSON objects legibly
 pp = pprint.PrettyPrinter(indent=4)
 
+
+@db_session
+def get_items(
+    page=1,
+    pagesize=1000000,
+):
+    q = select(
+        i for i in db.Item
+    )
+    total = count(q)
+    q_page = q.page(page, pagesize=pagesize)[:][:]
+    only = ('id', 'title', 'description')
+    data = [d.to_dict(only=only) for d in q_page]
+    return {
+        'data': data,
+        'page': page,
+        'pagesize': pagesize,
+        'total': total,
+        'num': len(data)
+    }
 # TODO functions
