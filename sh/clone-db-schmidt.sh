bbaa966eea9052233d62d95f239dc4c92ec8454e
mvanmaele <mvanmaele@talusanalytics.com>
2021-06-10 10:12:48 -0400

    Add shell scripts for data dump and restore

---
+++ sh/clone-db-schmidt.sh
@@ -0,0 +1,65 @@
+# Clone a AWS RDS database locally for the Health Security Net (aka. 
+# Schmidt) project
+# Arguments: [username] [aws_db_name] [local_db_name]
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+username=${1?Please provide your local pgsql server username as first argument};
+dblive=${2?Please provide the name of the AWS RDS Schmidt database you are cloning as second argument};
+dblocal=${3?Please provide the name of the local pgsql server database you are cloning to as third argument};
+
+printf "\n\n${bold}Cloning AWS RDS database locally...${normal}";
+now=$(date) && \
+printf "\nCurrent date: $now\n" &&
+
+# create local
+printf "\n\n${bold}Creating local database, if does not exist...\n${normal}";
+createdb $3;
+
+# backup local
+mkdir sh; cd sh && mkdir backup-local; cd backup-local && \
+pg_dump \
+--no-acl \
+--no-owner \
+--host "localhost" \
+--port "5432" \
+--username $username \
+--dbname $dblocal \
+-F d -f "$dblocal-local-$now" --verbose && \
+cd ../.. && \
+
+# backup aws
+printf "${bold}\n\nDumping AWS database...\n${normal}" && \
+mkdir sh; cd sh && mkdir backup-dev; cd backup-dev && \
+pg_dump \
+--no-acl \
+--no-owner \
+--host "schmidt.cc0kbkym7bvk.us-east-1.rds.amazonaws.com" \
+--port "5432" \
+--username "talus" \
+--dbname $dblive \
+-F d -f "$dbliveaws-$now" --verbose;
+cd ../.. && \
+
+# drop local
+printf "${bold}\n\nDropping local database data...\n${normal}" && \
+psql \
+--host "localhost" \
+--port "5432" \
+--username $username \
+--dbname $dblocal \
+-c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;" \
+
+# restore local from dev dump
+printf "${bold}\n\nRestoring AWS database to local database...\n${normal}" && \
+cd sh/backup-dev && \
+pg_restore --host "localhost" \
+--port "5432" \
+--username $username \
+--dbname $dblocal \
+--format=d \
+--verbose \
+"$dbliveaws-$now";
+
+# announce done
+printf "${bold}\n\nOperation completed successfully.\n\n${normal}";
---
+++ sh/update-schmidt-aws-rds-from-local.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+##
+# Update an RDS database using a local database.
+# 
+##
+
+programname=$0
+
+function usage {
+    echo "usage: $programname [username] [dblive] [dblocal]"
+    echo "  username    your local pgsql server username"
+    echo "  dblive      the name of the database on the Schmidt server you are updating"
+    echo "  dblocal     the name of the database on your local server with which you are updating"
+    exit 1
+}
+
+now=$(date);
+username=${1?Provide your local pgsql server username in first argument};
+dblive=${2?Provide the name of the database on the Schmidt server you are updating in second argument};
+dblocal=${3?Provide the name of the database on your local server with which you are updating in third argument};
+
+# dump local database
+echo "Current date: $now";
+mkdir sh; cd sh; mkdir backup-local; cd backup-local;
+pg_dump \
+--host "localhost" \
+--port "5432" \
+--username $1 \
+--dbname $3 --verbose \
+-F d -f "$now-local" && \
+cd ../..;
+
+# dump prod
+cd sh; mkdir backup-preview; cd backup-preview;
+pg_dump \
+--host "schmidt.cc0kbkym7bvk.us-east-1.rds.amazonaws.com" \
+--port "5432" \
+--username "talus" \
+--dbname $2 --verbose \
+-F d -f "$now-preview" && \
+cd ../..;
+
+# drop prod
+psql \
+--host "schmidt.cc0kbkym7bvk.us-east-1.rds.amazonaws.com" \
+--port "5432" \
+--username "talus" \
+--dbname $2 \
+-c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;" && \
+
+# restore prod from local dump
+cd sh/backup-local;
+pg_restore \
+--host "schmidt.cc0kbkym7bvk.us-east-1.rds.amazonaws.com" \
+--port "5432" \
+--username "talus" \
+--dbname $2 --verbose \
+--format=d --verbose "$now-local";
+cd ../.. && \
+
+# restart API server
+aws elasticbeanstalk restart-app-server --environment-name schmidt-api-preview \
+--region us-east-1 && \
+
+# invalidate frontend caches
+# prod and preview
+aws cloudfront create-invalidation \
+--distribution-id E3KCF50FO8RLIK \
+--paths "/*" && \
+aws cloudfront create-invalidation \
+--distribution-id E6J9FWVEWFDBG \
+--paths "/*";
