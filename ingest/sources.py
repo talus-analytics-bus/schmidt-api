a7bfade529ca137a0126f6c650a440fbd0402c8f
mvanmaele <mvanmaele@talusanalytics.com>
2021-07-14 11:55:05 -0400

    Format file

--- ingest/sources.py
+++ ingest/sources.py
@@ -14,18 +14,13 @@ from .util import bcolors
 pp = pprint.PrettyPrinter(indent=4)
 
 
-class DataSource():
+class DataSource:
     def __init__(self, name: str):
         self.name = name
 
 
 class AirtableSource(DataSource):
-    def __init__(
-        self,
-        name: str,
-        base_key: str,
-        api_key: str
-    ):
+    def __init__(self, name: str, base_key: str, api_key: str):
         DataSource.__init__(self, name)
         self.base_key = base_key
         self.api_key = api_key
@@ -34,7 +29,7 @@ class AirtableSource(DataSource):
         return self
 
     def workbook(self, key: str):
-        print('WARNING: `workbook` method not implemented for AirtableSource.')
+        print("WARNING: `workbook` method not implemented for AirtableSource.")
         return self
 
     def worksheet(self, name: str):
@@ -46,26 +41,35 @@ class AirtableSource(DataSource):
 
         except Exception as e:
             print(e)
-            print('\nFailed to open worksheet with name ' + str(name))
+            print("\nFailed to open worksheet with name " + str(name))
 
-    def as_dataframe(self, header_row: int = 0, view: str = None, max_records: int = 10000000):
+    def as_dataframe(
+        self,
+        header_row: int = 0,
+        view: str = None,
+        max_records: int = 10000000,
+    ):
         try:
-            records_tmp = self.ws.get_all(max_records=max_records) if view is None else \
-                self.ws.get_all(view=view)
+            records_tmp = (
+                self.ws.get_all(max_records=max_records)
+                if view is None
+                else self.ws.get_all(view=view)
+            )
             records = list()
             for r_tmp in records_tmp:
-                r = r_tmp['fields']
-                r['source_id'] = r_tmp['id']
+                r = r_tmp["fields"]
+                r["source_id"] = r_tmp["id"]
                 records.append(r)
 
             df = pd.DataFrame.from_records(records)
 
             print(
-                f'''{bcolors.OKGREEN}Found {len(df)} records in worksheet "{self.ws_name}"{bcolors.ENDC}''')
+                f"""{bcolors.OKGREEN}Found {len(df)} records in worksheet "{self.ws_name}"{bcolors.ENDC}"""
+            )
 
             # remove NaN values
-            df = df.replace(pd.np.nan, '', regex=True)
+            df = df.replace(pd.np.nan, "", regex=True)
             return df
         except Exception as e:
             print(e)
-            print('\nFailed to open worksheet')
+            print("\nFailed to open worksheet")
