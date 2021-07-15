51bb2e225859259a0a3094e630186750a2a99944
mvanmaele <mvanmaele@talusanalytics.com>
2021-07-15 10:57:13 -0400

    Make XLSX export files full screen

--- api/export/export.py
+++ api/export/export.py
@@ -13,10 +13,8 @@ import pandas as pd
 from .formats import WorkbookFormats
 
 
-class ExcelExport():
-    """Parent class for project-specific Excel export.
-
-    """
+class ExcelExport:
+    """Parent class for project-specific Excel export."""
 
     def __init__(self):
         return None
@@ -24,11 +22,12 @@ class ExcelExport():
     def build(self, **kwargs):
         # Create bytes output to return to client
         io = BytesIO()
-        writer = pd.ExcelWriter('temp.xlsx', engine='xlsxwriter')
+        writer = pd.ExcelWriter("temp.xlsx", engine="xlsxwriter")
         writer.book.filename = io
 
         # add a worksheet
         workbook = writer.book
+        workbook.set_size(3000, 3000)  # full screen
 
         # add content
         self.add_content(workbook, **kwargs)
@@ -45,11 +44,11 @@ class ExcelExport():
         return io
 
     def add_content(self):
-        print('Not implemented')
+        print("Not implemented")
         return False
 
 
-class SheetSettings():
+class SheetSettings:
     """Define settings for a workbook sheet to be written to an XLSX file.
 
     Parameters
@@ -102,9 +101,9 @@ class SheetSettings():
             Description of returned object.
 
         """
-        if self.type == 'data':
+        if self.type == "data":
             return 0
-        elif self.type == 'legend':
+        elif self.type == "legend":
             return 1
 
     def write_rows(self, worksheet, data):
@@ -125,7 +124,7 @@ class SheetSettings():
         """
         # if worksheet is legend, then always align text left
         is_legend = worksheet.name.startswith("Legend")
-        init_irow = self.init_irow['data']
+        init_irow = self.init_irow["data"]
         init_icol = self.get_init_icol()
         irow = init_irow
         icol = init_icol
@@ -134,16 +133,19 @@ class SheetSettings():
                 for colname in row[colgroup]:
                     value = row[colgroup][colname]
                     # special formatting
-                    if colname.endswith('date'):
+                    if colname.endswith("date"):
                         if value is None:
-                            value = 'Unspecified'
-                    elif value == 'Unspecified':
-                        value = ''
-                    cell_format = self.formats.cell() if is_legend or \
-                        colname not in ('HIV incidence', 'ART coverage (%)') \
+                            value = "Unspecified"
+                    elif value == "Unspecified":
+                        value = ""
+                    cell_format = (
+                        self.formats.cell()
+                        if is_legend
+                        or colname not in ("HIV incidence", "ART coverage (%)")
                         else self.formats.comma_num()
+                    )
 
-                    if colname == 'Year' and not is_legend:
+                    if colname == "Year" and not is_legend:
                         cell_format = self.formats.num_right()
 
                     worksheet.write(irow, icol, value, cell_format)
@@ -169,11 +171,11 @@ class SheetSettings():
             Description of returned object.
 
         """
-        init_irow = self.init_irow['colnames']
+        init_irow = self.init_irow["colnames"]
         init_icol = self.get_init_icol()
         irow = init_irow
         icol = init_icol
-        bg_colors = ['#E9EFF1', '#cddbe1']
+        bg_colors = ["#E9EFF1", "#cddbe1"]
         bg_color_idx = 0
         row = data[0]
         worksheet.set_row(irow, 40)
@@ -183,28 +185,28 @@ class SheetSettings():
             bg_color = bg_colors[bg_color_idx % 2]
             for colname in row[colgroup]:
                 worksheet.write(
-                    irow,
-                    icol,
-                    colname,
-                    self.formats.colname(bg_color)
+                    irow, icol, colname, self.formats.colname(bg_color)
                 )
-                if self.type == 'legend' and colname in (
-                    'Funders',
-                    'Sub-organization',
-                    'Authoring organization has governance authority?'
+                if self.type == "legend" and colname in (
+                    "Funders",
+                    "Sub-organization",
+                    "Authoring organization has governance authority?",
                 ):
                     worksheet.set_column(icol, icol, 60)
-                elif self.type == 'legend' and colname == 'Definition':
+                elif self.type == "legend" and colname == "Definition":
                     worksheet.set_column(icol, icol, 80)
-                elif self.type != 'legend' and colname == 'Title':
+                elif self.type != "legend" and colname == "Title":
                     worksheet.set_column(icol, icol, 25)
-                elif self.type != 'legend' and colname == 'Description':
+                elif self.type != "legend" and colname == "Description":
                     worksheet.set_column(icol, icol, 95)
-                elif self.type != 'legend' and colname in \
-                        ('Type of record', 'Date published'):
+                elif self.type != "legend" and colname in (
+                    "Type of record",
+                    "Date published",
+                ):
                     worksheet.set_column(icol, icol, 25)
-                elif self.type != 'legend' and colname in \
-                        ('Sortable date published'):
+                elif self.type != "legend" and colname in (
+                    "Sortable date published"
+                ):
                     worksheet.set_column(icol, icol, 30)
                 else:
                     worksheet.set_column(icol, icol, 50)
@@ -228,7 +230,7 @@ class SheetSettings():
             Description of returned object.
 
         """
-        init_irow = self.init_irow['colgroups']
+        init_irow = self.init_irow["colgroups"]
         init_icol = self.get_init_icol()
         irow = init_irow
         icol_end = init_icol
@@ -241,9 +243,12 @@ class SheetSettings():
                 icol_end = icol_end + 1
                 if (icol_end - icol_start + 1) == len(row[colgroup]):
                     worksheet.merge_range(
-                        irow, icol_start, irow, icol_end,
+                        irow,
+                        icol_start,
+                        irow,
+                        icol_end,
                         colgroup,
-                        self.formats.colgroup()
+                        self.formats.colgroup(),
                     )
 
     def write_legend_labels(self, worksheet):
@@ -261,18 +266,26 @@ class SheetSettings():
             Description of returned object.
 
         """
-        init_irow = self.init_irow['colnames']
+        init_irow = self.init_irow["colnames"]
         # worksheet.set_column(0, 0, 50)
         rows = [
-            (init_irow, 'Column name', self.formats.colname('#cddbe1')),
-            (init_irow + 1, 'Definition', self.formats.legend_cell()),
-            (init_irow + 2, 'Allowed values', self.formats.legend_cell()),
+            (init_irow, "Column name", self.formats.colname("#cddbe1")),
+            (init_irow + 1, "Definition", self.formats.legend_cell()),
+            (init_irow + 2, "Allowed values", self.formats.legend_cell()),
         ]
         for irow, text, cell_format in rows:
             worksheet.write(irow, 0, text, cell_format)
         worksheet.set_row(init_irow + 2, 360)
 
-    def write_header(self, worksheet, logo_fn, logo_offset, logo_stretch_correction, title, intro_text):
+    def write_header(
+        self,
+        worksheet,
+        logo_fn,
+        logo_offset,
+        logo_stretch_correction,
+        title,
+        intro_text,
+    ):
         """Write the sheet header, including title, subtitle, logo, etc.
 
         Parameters
@@ -292,21 +305,22 @@ class SheetSettings():
             Description of returned object.
 
         """
-        self.write_logo(worksheet,
-                        logo_fn,
-                        logo_offset,
-                        logo_stretch_correction,
-                        115)
+        self.write_logo(
+            worksheet, logo_fn, logo_offset, logo_stretch_correction, 115
+        )
         self.write_title(worksheet, title)
 
         today = date.today()
-        self.write_subtitle(worksheet, 'Downloaded on ' + str(today))
-        self.write_intro_text(
-            worksheet, intro_text)
+        self.write_subtitle(worksheet, "Downloaded on " + str(today))
+        self.write_intro_text(worksheet, intro_text)
 
     def write_logo(
-        self, worksheet, logo_fn, logo_offset,
-        logo_stretch_correction, row_height
+        self,
+        worksheet,
+        logo_fn,
+        logo_offset,
+        logo_stretch_correction,
+        row_height,
     ):
         """Add the logo at the specified filename path to the upper-left corner.
 
@@ -327,14 +341,16 @@ class SheetSettings():
         """
         worksheet.set_row(0, row_height)
         worksheet.insert_image(
-            0, 0, logo_fn,
+            0,
+            0,
+            logo_fn,
             {
-                'object_position': 3,
-                'y_scale': 1.1,
+                "object_position": 3,
+                "y_scale": 1.1,
                 # 'x_scale': logo_stretch_correction,
-                'x_offset': logo_offset['x_offset'],
-                'y_offset': logo_offset['y_offset'],
-            }
+                "x_offset": logo_offset["x_offset"],
+                "y_offset": logo_offset["y_offset"],
+            },
         )
 
     def write_title(self, worksheet, text):
@@ -353,7 +369,7 @@ class SheetSettings():
             Description of returned object.
 
         """
-        irow = self.init_irow['title']
+        irow = self.init_irow["title"]
         worksheet.write(irow, 0, text, self.formats.title())
 
     def write_subtitle(self, worksheet, text):
@@ -372,7 +388,7 @@ class SheetSettings():
             Description of returned object.
 
         """
-        irow = self.init_irow['subtitle']
+        irow = self.init_irow["subtitle"]
         worksheet.write(irow, 0, text, self.formats.subtitle())
 
     def write_intro_text(self, worksheet, text):
@@ -391,14 +407,12 @@ class SheetSettings():
             Description of returned object.
 
         """
-        irow = self.init_irow['intro_text']
+        irow = self.init_irow["intro_text"]
         icol_start = 0
         icol_end = 2
         worksheet.set_row(irow, 70)
         worksheet.merge_range(
-            irow, icol_start, irow, icol_end,
-            text,
-            self.formats.intro_text()
+            irow, icol_start, irow, icol_end, text, self.formats.intro_text()
         )
 
 
@@ -429,50 +443,50 @@ class GenericExcelExport(ExcelExport):
         self.db = db
         self.data = None
         self.init_irow = {
-            'logo': 0,
-            'title': 1,
-            'subtitle': 2,
-            'intro_text': 3,
-            'gap': 4,
-            'colgroups': 5,
-            'colnames': 6,
-            'data': 7
+            "logo": 0,
+            "title": 1,
+            "subtitle": 2,
+            "intro_text": 3,
+            "gap": 4,
+            "colgroups": 5,
+            "colnames": 6,
+            "data": 7,
         }
 
         # Define a sheet settings instance for each tab of the XLSX
         self.sheet_settings = [
             SheetSettings(
-                name='Exported data',
-                type='data',
-                intro_text='This is placeholder intro text for the main data sheet of the workbook. It can be edited by changing the `text` argument to the function `write_intro_text` in module `~/py/api/excel.py`.',
+                name="Exported data",
+                type="data",
+                intro_text="This is placeholder intro text for the main data sheet of the workbook. It can be edited by changing the `text` argument to the function `write_intro_text` in module `~/py/api/excel.py`.",
                 init_irow={
-                    'logo': 0,
-                    'title': 1,
-                    'subtitle': 2,
-                    'intro_text': 3,
-                    'gap': 4,
-                    'colgroups': 5,
-                    'colnames': 6,
-                    'data': 7
+                    "logo": 0,
+                    "title": 1,
+                    "subtitle": 2,
+                    "intro_text": 3,
+                    "gap": 4,
+                    "colgroups": 5,
+                    "colnames": 6,
+                    "data": 7,
                 },
-                data_getter=self.default_data_getter
+                data_getter=self.default_data_getter,
             ),
             SheetSettings(
-                name='Legend',
-                type='legend',
-                intro_text='This is a placeholder for a legend sheet.',
+                name="Legend",
+                type="legend",
+                intro_text="This is a placeholder for a legend sheet.",
                 init_irow={
-                    'logo': 0,
-                    'title': 1,
-                    'subtitle': 2,
-                    'intro_text': 3,
-                    'gap': 4,
-                    'colgroups': 5,
-                    'colnames': 6,
-                    'data': 7
+                    "logo": 0,
+                    "title": 1,
+                    "subtitle": 2,
+                    "intro_text": 3,
+                    "gap": 4,
+                    "colgroups": 5,
+                    "colnames": 6,
+                    "data": 7,
                 },
-                data_getter=self.default_data_getter_legend
-            )
+                data_getter=self.default_data_getter_legend,
+            ),
         ]
 
     def add_content(self, workbook):
@@ -500,24 +514,25 @@ class GenericExcelExport(ExcelExport):
 
             settings.write_header(
                 worksheet,
-                logo_fn='./api/assets/images/logo-talus.png',
+                logo_fn="./api/assets/images/logo-talus.png",
                 title=settings.name,
-                intro_text=settings.intro_text)
+                intro_text=settings.intro_text,
+            )
 
             data = settings.data
             settings.write_colgroups(worksheet, data)
             settings.write_colnames(worksheet, data)
             settings.write_rows(worksheet, data)
 
-            if settings.type == 'legend':
+            if settings.type == "legend":
                 settings.write_legend_labels(worksheet)
-            elif settings.type == 'data':
-                worksheet.freeze_panes(settings.init_irow['colnames'], 0)
+            elif settings.type == "data":
+                worksheet.freeze_panes(settings.init_irow["colnames"], 0)
                 worksheet.autofilter(
-                    settings.init_irow['colnames'],
+                    settings.init_irow["colnames"],
                     0,
-                    settings.init_irow['colnames'],
-                    settings.num_cols - 1
+                    settings.init_irow["colnames"],
+                    settings.num_cols - 1,
                 )
 
         return self
@@ -530,28 +545,24 @@ class GenericExcelExport(ExcelExport):
         # dictionaries with key = colname, value = data to show
         return [
             {
-                'Basic information':
-                {
-                    'Name': 'Test',
-                    'E-mail': 'Test',
+                "Basic information": {
+                    "Name": "Test",
+                    "E-mail": "Test",
                 },
-                'Additional details':
-                {
-                    'Hobbies': 'Test',
-                    'Favorite color': 'Test',
-                }
+                "Additional details": {
+                    "Hobbies": "Test",
+                    "Favorite color": "Test",
+                },
             },
             {
-                'Basic information':
-                {
-                    'Name': 'Test',
-                    'E-mail': 'Test',
+                "Basic information": {
+                    "Name": "Test",
+                    "E-mail": "Test",
                 },
-                'Additional details':
-                {
-                    'Hobbies': 'Test',
-                    'Favorite color': 'Test',
-                }
+                "Additional details": {
+                    "Hobbies": "Test",
+                    "Favorite color": "Test",
+                },
             },
         ]
 
@@ -563,27 +574,23 @@ class GenericExcelExport(ExcelExport):
         # dictionaries with key = colname, value = data to show
         return [
             {
-                'Basic information':
-                {
-                    'Name': 'Test',
-                    'E-mail': 'Test',
+                "Basic information": {
+                    "Name": "Test",
+                    "E-mail": "Test",
                 },
-                'Additional details':
-                {
-                    'Hobbies': 'Test',
-                    'Favorite color': 'Test',
-                }
+                "Additional details": {
+                    "Hobbies": "Test",
+                    "Favorite color": "Test",
+                },
             },
             {
-                'Basic information':
-                {
-                    'Name': 'Test',
-                    'E-mail': 'Test',
+                "Basic information": {
+                    "Name": "Test",
+                    "E-mail": "Test",
                 },
-                'Additional details':
-                {
-                    'Hobbies': 'Test',
-                    'Favorite color': 'Test',
-                }
+                "Additional details": {
+                    "Hobbies": "Test",
+                    "Favorite color": "Test",
+                },
             },
         ]
