de73b12c604d2fcd42edbf40721ebe0f23960c2d
mvanmaele <mvanmaele@talusanalytics.com>
2022-01-06 11:12:41 -0500

    Output lists of newly added or removed IDs

--- sh/get-ids-for-comparison.sh
+++ sh/get-ids-for-comparison.sh
@@ -4,5 +4,7 @@ cd $BASEDIR;
 
 psql schmidt -c "select id from item order by 1;" > ./tmp/new_ids.txt && \
 psql schmidt -c "select id from item order by 1;" --host schmidt.cc0kbkym7bvk.us-east-1.rds.amazonaws.com -U talus  > ./tmp/old_ids.txt;
+comm -23 new_ids.txt old_ids.txt > ./tmp/ids_in_new_not_old.txt && \
+comm -23 old_ids.txt new_ids.txt > ./tmp/ids_in_old_not_new.txt;
 
 cd $ORIGDIR;
